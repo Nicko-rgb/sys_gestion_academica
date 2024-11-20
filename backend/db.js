cd0651e5148f1013985c9928708092913b78c832
Solsolperez <jhoandrisolsolperez@gamil.com>
2024-11-20 16:31:36 -0500

    cambiosSolSol

--- backend/db.js
+++ backend/db.js
@@ -1,27 +1,31 @@
-// // db.js
+// db.js
+const { Sequelize } = require('sequelize');
+require('dotenv').config(); // Cargar las variables de entorno
+
+// Obtiene la URL de conexión desde las variables de entorno
+const db = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    logging: false,
+});
+
+// Verifica la conexión
+db.authenticate()
+    .then(() => {
+        console.log('Conexión a la base de datos establecida correctamente.');
+    })
+    .catch(err => {
+        console.error('Error al conectar a la base de datos:', err.message);
+        console.error(err);
+    });
+
+process.on('unhandledRejection', (reason, promise) => {
+    console.error('Unhandled Rejection at:', promise, 'reason:', reason);
+});
+module.exports = db;
 // const { Sequelize } = require('sequelize');
 
-// // Configura la conexión a la base de datos PostgreSQL
-// const db = new Sequelize('gestion_academica', 'postgres', 'solsol03', {
-//     host: 'localhost',
-//     dialect: 'postgres',
+// const sequelize = new Sequelize('postgres://postgres:solsol03@localhost:5433/gestion_academica',{
 //     logging: false
-// });
+// })
 
-// // Verifica la conexión
-// db.authenticate()
-//     .then(() => {
-//         console.log('Conexión a la base de datos establecida correctamente.');
-//     })
-//     .catch(err => {
-//         console.error('No se pudo conectar a la base de datos:', err);
-//     });
-
-// module.exports = db;
-const { Sequelize } = require('sequelize');
-
-const sequelize = new Sequelize('postgres://postgres:solsol03@localhost:5433/gestion_academica',{
-    logging: false
-})
-
-module.exports=sequelize
+// module.exports=sequelize
